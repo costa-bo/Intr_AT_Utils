d43e477c2a5669d87a4c4f5ccf3bfd51a2c769b4
konstantinos Bogiatzakis <konstantinos.bogiatzakis@nokia.com>
2021-04-14 13:03:04 +0300

    initial commit

---
+++ src/main/java/com/mypackage/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mypackage;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void costa(){
+        System.out.println("Hello Costa!");
+    }
+}
